6161a398ad1c0d7b784af871d2dada4333302ec9
shlomi123002 <shlomi123002@gmail.com>
2024-01-26 11:30:57 +0200

    Shlomi.java was updated

--- Shlomi.java
+++ Shlomi.java
@@ -1,7 +1,7 @@
 public class Shlomi{
 	public static void main(String[] args){
 		for(int i=1 ; i<=10;i++){
-			System.out.println("Shlomi ......"+i);
+			System.out.println("Thanks moshe ......"+i);
 		}	
 	}
 }	
