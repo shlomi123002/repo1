1f0a6f2bf7e1669678f88ac743c78382dd417e6e
shlomi123002 <shlomi123002@gmail.com>
2024-01-26 10:36:09 +0200

    Hello.java was updated

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 	public static void main(String[] args){
 		for(int i=1 ; i<=10;i++){
-			System.out.println("hello world..."+i);
+			System.out.println("Shlomi the king..."+i);
 		}	
 	}
 }	
